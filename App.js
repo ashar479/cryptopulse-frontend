36740d8b6486edd3c7aba5639f285ab2fb173480
ashar479 <ashar479@asu.edu>
2025-07-17 22:49:05 -0700

    logs solved

--- App.js
+++ App.js
@@ -6,13 +6,34 @@ import CoinDetailScreen from './screens/CoinDetailScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import InvestmentsScreen from './screens/InvestmentsScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 function MainTabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Investments') {
+            iconName = focused ? 'trending-up' : 'trending-up-outline';
+          } else if (route.name === 'Favorites') {
+            iconName = focused ? 'star' : 'star-outline';
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'settings' : 'settings-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#d1b12a', // active color
+        tabBarInactiveTintColor: 'gray', // inactive color
+      })}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Investments" component={InvestmentsScreen} />
       <Tab.Screen name="Favorites" component={FavoritesScreen} />
